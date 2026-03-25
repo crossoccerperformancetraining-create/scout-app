8574314759372afd1d75a81f60faca8671b25ec5
crossoccerperformancetraining-create <crossoccerperformancetraining@gmail.com>
2026-03-25 20:24:05 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,35 @@
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
+const CACHE_NAME = 'scout-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js',
+  'https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js',
+  'https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
+];
+
+// Instala o Service Worker e salva os arquivos no cache do celular
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
-self.addEventListener('fetch', (e) => {
-  // Pass through
+
+// Intercepta as requisições: se não tiver internet, pega do Cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retorna o arquivo do cache se existir
+        if (response) {
+          return response;
+        }
+        // Se não, tenta baixar da internet
+        return fetch(event.request);
+      })
+  );
 });
