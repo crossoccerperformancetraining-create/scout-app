295d8209e76331d6ab5de1d0c6f73e2f30ae7757
crossoccerperformancetraining-create <crossoccerperformancetraining@gmail.com>
2026-08-12 10:18:54 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'scout-intelligence-v73-1-13-reports360';
+const CACHE_NAME = 'scout-intelligence-v73-1-16-potencial-executivo';
 const BASE_URL = new URL('./', self.location.href);
 const OFFLINE_URL = new URL('index.html', BASE_URL).href;
 
